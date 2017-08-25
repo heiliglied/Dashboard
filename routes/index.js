80a705806ba2c705b1bbe5e386651c638ddca772
heiliglied <heiliglied@gmail.com>
2017-08-25 23:43:20 +0900

    router add

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* moon router */
+router.get('/moon', function (req, res, next) {
+    res.send('Release');
+});
+
 module.exports = router;
