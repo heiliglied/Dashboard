0040a54580ebab4b9c596449e85a5391ce9884fa
Jeong-jae <n_nflowers@naver.com>
2017-09-01 15:09:29 +0900

    게시판 일부 제작

--- routes/lee.js
+++ routes/lee.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const boardController = require('../controller/Lee/boardController')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.send('lee');
-});
+router.get('/', boardController.indexBoard);
+router.get('/write', boardController.writeBoard);
+
+/*router.get('/', function (req, res, next) {
+    res.render(boardController.getIndex);
+});*/
 
 module.exports = router;
